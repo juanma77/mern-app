f157e5adf5faae3919ee048d17fbc55a6f4d1fa5
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-07-17 23:45:51 -0500

    port settings added

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.listen(8000, () =>{
-    console.log('Server listenning on port 8000!')
+// Port settings
+app.set( 'port', process.env.PORT || 8000 ); 
+
+
+
+// Usamos npm run dev para iniciar el server de desarrollo; como no es un script de start, no podemos usar el npm start o npm dev
+app.listen( app.get('port') , () =>{
+    console.log(`Server listenning on port ${ app.get('port') }!`);
 });
